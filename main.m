cf8e7eeb1487a1ab1f0855f8502d7548e06fd83a
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2022-01-20 14:09:22 -0500

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 clear, close all, clc
 %% Important: Are you using the Merlin, the Cleanroom SEM or other Instrument? ...
-% Mode=0 - BETA - Automatic detection of scalebar and text - 
+% Mode 0 - BETA - Automatic detection of scalebar and text - 
 % Mode 1 - Automatic detection of scalebar but not text - enter scale value manually
 % Mode 2 - Manual input of both scalebar and text
 
 Mode        = 0;
-Instrument  = 3; % Please select the your SEM: Merlin=1, Zeiss SEM = 2 if other no effect
-Scale       = 1; % In Microns - if Mode 1 or 2 enter the scalebar size - ie How many microns is the scalebar? (makes no difference for mode 0)
+Instrument  = 3; % Please select the your SEM: Merlin = 1, Jeol = 2, Zeiss (Pitt) = 3 if other no effect
+Scale       = 0; % Size of scalebar in Microns (if mode is 0 then the scalebar is automatically detected, so no need to put a value)
 Save        = 1; % Do you want to save the image after you run the script? 0 for No, 1 for Yes
 %% Run Image Loop
 [ImageNames, path] = uigetfile('*.*','MultiSelect','on');
