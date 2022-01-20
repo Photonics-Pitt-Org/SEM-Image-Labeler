51a03cb367d973039ecd484ecdc0b7bfb964bd18
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2022-01-20 14:03:58 -0500

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ clear, close all, clc
 % Mode 2 - Manual input of both scalebar and text
 
 Mode        = 0;
-Instrument  = 3; % Please select the your SEM: Merlin=1, Cleanroom SEM = 2 if other no effect
+Instrument  = 3; % Please select the your SEM: Merlin=1, Zeiss SEM = 2 if other no effect
 Scale       = 1; % In Microns - if Mode 1 or 2 enter the scalebar size - ie How many microns is the scalebar? (makes no difference for mode 0)
 Save        = 1; % Do you want to save the image after you run the script? 0 for NO - 1 for Yes
 %% Run Image Loop
